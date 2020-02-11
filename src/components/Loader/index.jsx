3f9877ef8adf2d3154958b0a4f5352547377724d
tms1337 <faruk1337@gmail.com>
2020-02-11 11:13:18 +0100

    Add loading gif to <Loader/>

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Loader = () => {
-  return <>Loading...</>;
+  return (
+    <img
+      style={styles.img}
+      src="https://media3.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+      alt="data is loading"
+    />
+  );
 };
+
+const styles = {
+  img: {
+    width: "50px"
+  }
+}
 
 export default Loader;
