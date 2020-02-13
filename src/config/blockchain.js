379cb764d6beda4cfa6473785cf9fd7e09e7f38b
tms1337 <faruk1337@gmail.com>
2020-02-13 11:47:27 +0100

    Change tokenApiUrl to https

--- src/config/blockchain.js
+++ src/config/blockchain.js
@@ -4,6 +4,6 @@ const nodeConfig = {
 
 const contractAddress = "0xFF5A7299ff6f0fbAad9b38906b77d08c0FBdc9A7";
 
-const tokenApiUrl = "http://api.ethplorer.io/getAddressInfo";
+const tokenApiUrl = "https://api.ethplorer.io/getAddressInfo";
 
 export { nodeConfig, contractAddress, tokenApiUrl };
